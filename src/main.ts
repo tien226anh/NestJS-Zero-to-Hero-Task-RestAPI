d61484d07e7a3cedbab16d00b6d09764ffa90df0
tien226anh <tien226anh@gmail.com>
2023-06-24 20:09:07 +0700

    swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Udemy-NestJS')
+    .setDescription('Udemy-NestJS')
+    .setVersion('1.0')
+    .addTag('nestjs')
+    .build();
+  const doc = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, doc);
   await app.listen(3000);
 }
 bootstrap();
